34c40fdd1e51a35ba8624163f2280867df69cd3b
Kevin-Druis Merenda <kdmerenda@gmail.com>
2017-09-13 17:56:44 -0600

    a working NN on GPU, with stats on images

--- firstConvolutionELVES.lua
+++ firstConvolutionELVES.lua
@@ -9,39 +9,52 @@ dataPath = "/media/kswiss/ExtraDrive1/ELVESNet/merged/"
 dataAll = torch.load(dataPath..'ALLDataImage.dat')
 labelAll = torch.load(dataPath..'ALLDataLocation.dat')
 
---insert a singleton in position 2
+--insert a singleton in position 2 to add the dimension of the image channel to the dataset
 modelSoliton = nn.Unsqueeze(2)
 dataAll = modelSoliton:forward(dataAll)
-print(dataAll:size())
+--print(dataAll:size())
 
---9781 to play with
+--9781 to play with --- later found two unsorted elves due to bad latitude values in label. Need to remove them from dataset. 6675 6153. done later. Also ran out of memeory with a train set of 8000
 trsize = 7000
 tesize = 1000
 
---gpu work
-cuda = false
+--fixing the problem
+data1 = dataAll[{ {1,6153-1},{},{} }]
+label1 = labelAll[{ {1,6153-1},{} }]
+data2 = dataAll[{ {6153+1,6675-1},{},{} }]
+label2 = labelAll[{ {6153+1,6675-1},{} }]
+data3 = dataAll[{ {6675+1,dataAll:size()[1]},{},{} }]
+label3 = labelAll[{ {6675+1,dataAll:size()[1]},{} }]
 
---net input parameters
+dataAll = torch.cat(data1,data2,1):cat(data3,1)
+labelAll = torch.cat(label1,label2,1):cat(label3,1)
+--9779 ELVES left to play with.
+
+--gpu work
+cuda = true
+
+--net input parameters ---> THIS NEEDS TO BE STUDIED
 n_inputs_1 = 1 --number of input planes to layer 1
 n_filters_1 = 16 --number of convolutional filters to layer 1
 n_inputs_2 = n_filters_1 --number of input planes to layer 2
 n_filters_2 = 128 --number of convolutional filters to layer 2
 n_convx = 2 --conv filter size - use square filter for now? image not square.
 n_convy = 2 --conv filter size - use square filter for now? image not square.
-n_pool = 2 --size of pooling filter
+n_pool = 2 --size of max pooling filter
 n_pool_dx = 2 --size of steps by which to move filter.
 
-print_every = 1
-nb_epoch = 5
+learningRateValue = 1e-1
+print_every = 10
+nb_epoch = 100
 
 
---need to redefine the labels based on discrete grid. For first attempt, define 4 sections instead of continuous possibility of values. Here, the elves were created at random in the following range:   float lowestLON=-68, highestLON=-62;  float lowestLAT=-37, highestLAT=-32; If we discretize a possible grid in which the ELVES can be (eg 4 sections) then all the labels have to be redefined to the number of a given section (eg: 1,2,3,4). For starters, the numbering will be done wrt to the geodetic coordinates (earth cs) not wrt to the detector. I believe this will be useful for when the full detector is used. Define nSections as the number of discrete sections in which an ELVES can be.
+--need to redefine the labels based on discrete grid. For first attempt, define 4/9 sections instead of continuous possibility of values. Here, the elves were created at random in the following range:   float lowestLON=-68, highestLON=-62;  float lowestLAT=-37, highestLAT=-32; If we discretize a possible grid in which the ELVES can be (eg 4 sections) then all the labels have to be redefined to the number of a given section (eg: 1,2,3,4). For starters, the numbering will be done wrt to the geodetic coordinates (earth cs) not wrt to the detector. I believe this will be useful for when the full detector is used. Define nSections as the number of discrete sections in which an ELVES can be.
 latLow = -37
 latHigh = -32
 lonLow = -68
 lonHigh = -62
-nSectionsLat = 2
-nSectionsLon = 2
+nSectionsLat = 3
+nSectionsLon = 3
 nSectionsTot = nSectionsLat*nSectionsLon
 --making  rectangular grid, loop through i first and j next. 
 -- latLow + i*(abs(latLow-latHigh) / nSectionsLat)
@@ -55,11 +68,11 @@ nSectionsTot = nSectionsLat*nSectionsLon
 --               |     |     |     |     |                    |           |           |     
 --               |-----|-----|-----|-----|                    |     1     |     2     |     
 --               |     |     |     |     |                    |           |           |     
---               |  5  |  6  |  7  |  8  |                    |           |           |     
---               |     |     |     |     |                    |           |           |     
---               |-----|-----|-----|-----|                    |-----------|-----------|     
---               |     |     |     |     |                    |           |           |
---               |  9  | 10  | 11  | 12  |                    |           |           |
+--   /           |  5  |  6  |  7  |  8  |                    |           |           |     
+-- /             |     |     |     |     |                    |           |           |     
+--|PAO           |-----|-----|-----|-----|                    |-----------|-----------|     
+-- \             |     |     |     |     |                    |           |           |
+--   \           |  9  | 10  | 11  | 12  |                    |           |           |
 --               |     |     |     |     |                    |           |           |
 --               |-----|-----|-----|-----|                    |     3     |     4     |
 --               |     |     |     |     |                    |           |           |
@@ -91,7 +104,8 @@ for k=1,labelAllReworked:size()[1] do
 end
 
 --gnuplot.hist(labelAllReworked,nSectionsTot*10)   
-print("Number of uncategorized ELVES: " .. torch.sum(torch.eq(labelAllReworked,0)) .. "\n\n\n")
+print("Number of uncategorized ELVES: " .. torch.sum(torch.eq(labelAllReworked,0)))
+
 
 --save the data in a struct and format the train and test sets to get part of the full dataset
 train_data = {
@@ -105,9 +119,9 @@ test_data = {
    size = function() return tesize end
 }
 
-print(train_data.data:size())
-print(train_data.size(), " Location of a train ELVES (Section): ",train_data.labels[2])
-print(test_data.size(), " Location of a test ELVES (Section): ",test_data.labels[2])
+--print(train_data.data:size())
+print(train_data.size(), " ELVES in train dataset. Location of a train ELVES (Section): ",train_data.labels[2])
+print(test_data.size(), " ELVES in test dataset.")
 
 -- all images here are the integrated signal in time. So take one of the 9781 images and test a possible comvolution on a 'grayscale'
 --imageSample = torch.Tensor(1,dataAll:size()[2],dataAll:size()[3])
@@ -140,8 +154,7 @@ model:add(nn.SpatialConvolution(n_inputs_1,n_filters_1,n_convx,n_convy))
 --Transfer Function Layer:
 ---------------------------
 --introduce a non-linearity after a parametrized layer. This is the main computation performed by the artificial neuron. The activation function z_i = f(x,w_i) and the output function y_i = f(z_i) are summed up with the term transfer functions.
-
-model:add(nn.Tanh()) --f(x) = (exp(x) - exp(-x)) / (exp(x) + exp(-x))
+model:add(nn.Tanh())  --f(x) = (exp(x) - exp(-x)) / (exp(x) + exp(-x))
 --model:add(nn.ReLU()) --f(x) = max(0, x)
 --model:add(nn.Sigmoid()) --f(x) = 1 / (1 + exp(-x))
 --model:add(nn.SoftMax()) --f_i(x) = exp(x_i - shift) / sum_j exp(x_j - shift)
@@ -157,28 +170,28 @@ model:add(nn.SpatialMaxPooling(n_pool,n_pool,n_pool_dx,n_pool_dx));
 --Miscellanous Layer:
 ---------------------------
 --During training, Dropout masks parts of the input using binary samples from a bernoulli distribution. Each input element has a probability of p of being dropped, i.e having its commensurate output element be zero. This has proven an effective technique for regularization and preventing the co-adaptation of neurons
-model:add(nn.Dropout(0.50));
+--model:add(nn.Dropout(0.50));
 
 --============
 --SECOND LAYER
 --============
 model:add(nn.SpatialConvolution(n_inputs_2,n_filters_2,n_convx,n_convy))
---model:add(nn.Tanh())
-model:add(nn.ReLU()) 
+model:add(nn.Tanh()) 
 model:add(nn.SpatialMaxPooling(n_pool,n_pool,n_pool_dx,n_pool_dx));
-model:add(nn.Dropout(0.25));
+--model:add(nn.Dropout(0.25));
+
 
 --===========
 --THIRD LAYER
 --===========
---this is where the complex space is simplified back to a linear value. Applies a linear transformation to the incoming data, i.e. y = Ax + b. The input tensor given in forward(input) must be either a vector (1D tensor) or matrix (2D tensor)
+--Fully connected layer: this is where the complex space is simplified back to a linear value. Applies a linear transformation to the incoming data, i.e. y = Ax + b. The input tensor given in forward(input) must be either a vector (1D tensor) or matrix (2D tensor)
 --model:add(nn.Reshape(n_filters_2*n_convx*n_convy))
 --model:add(nn.Linear(n_filters_2*n_convx*n_convy, n_filters_2*n_convx*n_convy))
 model:add(nn.Reshape(128*4*14))
 model:add(nn.Linear(n_filters_2*n_convx*n_convy*14, n_filters_2*n_convx*n_convy))
---model:add(nn.Tanh())
-model:add(nn.ReLU()) 
-model:add(nn.Dropout(0.5));
+model:add(nn.Tanh())
+--model:add(nn.ReLU()) 
+--model:add(nn.Dropout(0.5));
 model:add(nn.Linear(n_filters_2*n_convx*n_convy, nSectionsTot))
 model:add(nn.LogSoftMax())
 
@@ -187,14 +200,6 @@ model:add(nn.LogSoftMax())
 --==============
 --local criterion = nn.ClassNLLCriterion()
 local criterion = nn.CrossEntropyCriterion()
-
---======
--- TRAIN
---======
---taken from practical 4 of oxford class
-local params, grads = model:getParameters()
---initialize weights. needed?
-params:uniform(-0.01, 0.01)
 
 --===========
 --CUDA
@@ -208,7 +213,13 @@ if(cuda) then
    criterion = criterion:cuda()
 end
 
-
+--======
+-- TRAIN
+--======
+--taken from practical 4 of oxford class
+local params, grads = model:getParameters()
+--initialize weights. needed?
+params:uniform(-0.01, 0.01)
 
 --===========
 --OPTIMIZER
@@ -233,7 +244,7 @@ end
 -- optimization loop
 --do the training optimization over many epochs. 
 local losses = {}
-local optim_state = {learningRate = 1e-1}
+local optim_state = {learningRate = learningRateValue}
 for i = 1, nb_epoch do
    local _, loss = optim.adagrad(feval, params, optim_state)
    losses[#losses + 1] = loss[1] -- append the new loss
@@ -253,24 +264,90 @@ gnuplot.plot({'losses',
 )
 
 
-print("The NN: \n",string.format( model))
+print("The NN: \n",model)
 
 --classification error on train set
 local log_probs = model:forward(train_data.data)
 local _, predictions = torch.max(log_probs, 2)
-print('# correct for train set:')
+print('\n# correct for train set:')
 print(torch.mean(torch.eq(predictions:long(), train_data.labels:long()):double()))
 
 --classification error on test set
 local log_probs = model:forward(test_data.data)
 local _, predictions = torch.max(log_probs, 2)
-print('# correct for test set:')
+print('\n# correct for test set:')
 print(torch.mean(torch.eq(predictions:long(), test_data.labels:long()):double()))
 
+--print(model:get(1).output:size(),model:get(1).weight:size())--,mod.output:size(),modSpatPool1.output:size(),modSpatPool1.indices:size())--modSpatConv2:size(),modTranFu
+--image.display(model:get(1).output)
+--image.display(model:get(1).weight)
 
---res = net:forward(imageSample)
-print("\n\n")
---print(res:size())
---image.display(res)
+print("\nimage 13 truth ", test_data.labels[13])
+outputopt1 = model:forward(test_data.data[13])
+local confidences1, indices1 = torch.sort(outputopt1, true)
+print("\nimage 13 guess ", indices1[1])
+for i=1,outputopt1:size(1) do
+   print(i,outputopt1[i])
+end
 
+--second way to check percent correct based....
+correct = 0
+for i=1,test_data.size() do
+   local groundtruth = test_data.labels[i]
+   local prediction = model:forward(test_data.data[i])
+   local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+   if groundtruth == indices[1] then
+      correct = correct + 1
+   end
+end
+print("\nCorrect: ",correct, 100*correct/test_data.size() .. ' % ')
 
+class_performance = torch.Tensor(nSectionsTot):zero()
+class_prediction = torch.Tensor(nSectionsTot):zero()
+class_truth = torch.Tensor(nSectionsTot):zero()
+for i=1,test_data.size() do
+   local groundtruth = test_data.labels[i]
+   local prediction = model:forward(test_data.data[i])
+   local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+   if groundtruth == indices[1] then
+      class_performance[groundtruth] = class_performance[groundtruth] + 1
+   end
+   class_truth[groundtruth] = class_truth[groundtruth] + 1
+   class_prediction[indices[1]] = class_prediction[indices[1]] + 1
+end
+for i=1,nSectionsTot do
+   print(i, 100*class_performance[i]/test_data.size() .. ' %')
+end
+gnuplot.figure()
+gnuplot.plot({'Prediction',class_prediction},{'Truth',class_truth})
+--gnuplot.raw('replot')
+
+gnuplot.figure()
+gnuplot.imagesc(model:get(1).output[13]);
+gnuplot.raw("set xrange restore")
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.raw('replot')
+
+gnuplot.figure()
+gnuplot.imagesc(model:get(3).output[13]);
+
+gnuplot.figure()
+gnuplot.imagesc(model:get(4).output[13]);
+gnuplot.figure()
+gnuplot.imagesc(model:get(6).output[13]);
+
+--testing another optimization library. 
+--trainer = nn.StochasticGradient(model, criterion)
+--grads:zero()
+--trainer.learningRate = 0.001
+--trainer.maxIteration = 5
+--trainer:train(train_data.data)
+--outputopt2 = model:forward(test_data.data[13])
+--print(outputopt2)
+--how is the classes output ordered?
+--for i=1,outputopt2:size(1) do
+--   print(i, predicted[i])
+--end
