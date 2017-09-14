1e8d18fd755fe9042dff249d34dc34a49a643e56
Kevin-Druis Merenda <kdmerenda@gmail.com>
2017-09-14 15:50:06 -0600

    90% on 12x12 grid, dropouts and and plots added.

--- firstConvolutionELVES.lua
+++ firstConvolutionELVES.lua
@@ -14,8 +14,8 @@ modelSoliton = nn.Unsqueeze(2)
 dataAll = modelSoliton:forward(dataAll)
 --print(dataAll:size())
 
---9781 to play with --- later found two unsorted elves due to bad latitude values in label. Need to remove them from dataset. 6675 6153. done later. Also ran out of memeory with a train set of 8000
-trsize = 7000
+--9781 to play with --- later found two unsorted elves due to bad latitude values in label. Need to remove them from dataset. 6675 6153. done later. Also ran out of memory with a train set of 8000
+trsize = 1000
 tesize = 1000
 
 --fixing the problem
@@ -38,14 +38,14 @@ n_inputs_1 = 1 --number of input planes to layer 1
 n_filters_1 = 16 --number of convolutional filters to layer 1
 n_inputs_2 = n_filters_1 --number of input planes to layer 2
 n_filters_2 = 128 --number of convolutional filters to layer 2
-n_convx = 2 --conv filter size - use square filter for now? image not square.
-n_convy = 2 --conv filter size - use square filter for now? image not square.
+n_convx = 4 --conv filter size - use square filter for now? image not square.
+n_convy = 4 --conv filter size - use square filter for now? image not square.
 n_pool = 2 --size of max pooling filter
-n_pool_dx = 2 --size of steps by which to move filter.
+n_pool_dx = n_pool --size of steps by which to move filter.
 
-learningRateValue = 1e-1
+learningRateValue = 1e-2
 print_every = 10
-nb_epoch = 100
+nb_epoch = 500
 
 
 --need to redefine the labels based on discrete grid. For first attempt, define 4/9 sections instead of continuous possibility of values. Here, the elves were created at random in the following range:   float lowestLON=-68, highestLON=-62;  float lowestLAT=-37, highestLAT=-32; If we discretize a possible grid in which the ELVES can be (eg 4 sections) then all the labels have to be redefined to the number of a given section (eg: 1,2,3,4). For starters, the numbering will be done wrt to the geodetic coordinates (earth cs) not wrt to the detector. I believe this will be useful for when the full detector is used. Define nSections as the number of discrete sections in which an ELVES can be.
@@ -53,8 +53,8 @@ latLow = -37
 latHigh = -32
 lonLow = -68
 lonHigh = -62
-nSectionsLat = 3
-nSectionsLon = 3
+nSectionsLat = 12
+nSectionsLon = 12
 nSectionsTot = nSectionsLat*nSectionsLon
 --making  rectangular grid, loop through i first and j next. 
 -- latLow + i*(abs(latLow-latHigh) / nSectionsLat)
@@ -170,7 +170,7 @@ model:add(nn.SpatialMaxPooling(n_pool,n_pool,n_pool_dx,n_pool_dx));
 --Miscellanous Layer:
 ---------------------------
 --During training, Dropout masks parts of the input using binary samples from a bernoulli distribution. Each input element has a probability of p of being dropped, i.e having its commensurate output element be zero. This has proven an effective technique for regularization and preventing the co-adaptation of neurons
---model:add(nn.Dropout(0.50));
+model:add(nn.Dropout(0.50));
 
 --============
 --SECOND LAYER
@@ -178,7 +178,7 @@ model:add(nn.SpatialMaxPooling(n_pool,n_pool,n_pool_dx,n_pool_dx));
 model:add(nn.SpatialConvolution(n_inputs_2,n_filters_2,n_convx,n_convy))
 model:add(nn.Tanh()) 
 model:add(nn.SpatialMaxPooling(n_pool,n_pool,n_pool_dx,n_pool_dx));
---model:add(nn.Dropout(0.25));
+model:add(nn.Dropout(0.25));
 
 
 --===========
@@ -187,12 +187,16 @@ model:add(nn.SpatialMaxPooling(n_pool,n_pool,n_pool_dx,n_pool_dx));
 --Fully connected layer: this is where the complex space is simplified back to a linear value. Applies a linear transformation to the incoming data, i.e. y = Ax + b. The input tensor given in forward(input) must be either a vector (1D tensor) or matrix (2D tensor)
 --model:add(nn.Reshape(n_filters_2*n_convx*n_convy))
 --model:add(nn.Linear(n_filters_2*n_convx*n_convy, n_filters_2*n_convx*n_convy))
-model:add(nn.Reshape(128*4*14))
-model:add(nn.Linear(n_filters_2*n_convx*n_convy*14, n_filters_2*n_convx*n_convy))
+------manual input of reshape... don't understand why the numbers are like that. These are given to me in error when running code.  need to understand this. 
+model:forward(train_data.data[1]) ---to get the model size at the stage you need to pass one image through. 
+reshapeInputValues = model:get(7).output:size()[1] * model:get(7).output:size()[2] * model:get(7).output:size()[3]
+
+model:add(nn.Reshape(reshapeInputValues))
+model:add(nn.Linear(reshapeInputValues, 200))
 model:add(nn.Tanh())
 --model:add(nn.ReLU()) 
---model:add(nn.Dropout(0.5));
-model:add(nn.Linear(n_filters_2*n_convx*n_convy, nSectionsTot))
+model:add(nn.Dropout(0.5));
+model:add(nn.Linear(200, nSectionsTot))
 model:add(nn.LogSoftMax())
 
 --==============
@@ -255,14 +259,15 @@ for i = 1, nb_epoch do
 end
 
 print(params:size()) --I assume these can be saved so that the net doesn't have to be retrained.
-
-gnuplot.figure()
+--PLOT LOSSES
+gnuplot.pngfigure("loss.png")
 gnuplot.plot({'losses',
   torch.range(1, #losses), -- x-coordinates
   torch.Tensor(losses),    -- y-coordinates
   '-'}
 )
-
+gnuplot.raw("replot")
+gnuplot.close()
 
 print("The NN: \n",model)
 
@@ -278,9 +283,6 @@ local _, predictions = torch.max(log_probs, 2)
 print('\n# correct for test set:')
 print(torch.mean(torch.eq(predictions:long(), test_data.labels:long()):double()))
 
---print(model:get(1).output:size(),model:get(1).weight:size())--,mod.output:size(),modSpatPool1.output:size(),modSpatPool1.indices:size())--modSpatConv2:size(),modTranFu
---image.display(model:get(1).output)
---image.display(model:get(1).weight)
 
 print("\nimage 13 truth ", test_data.labels[13])
 outputopt1 = model:forward(test_data.data[13])
@@ -315,39 +317,127 @@ for i=1,test_data.size() do
    class_truth[groundtruth] = class_truth[groundtruth] + 1
    class_prediction[indices[1]] = class_prediction[indices[1]] + 1
 end
+print("classe #   truth #  prediction #  performance #  perf/truth (%)")
 for i=1,nSectionsTot do
-   print(i, 100*class_performance[i]/test_data.size() .. ' %')
+   print("   ",i, class_truth[i], class_prediction[i], class_performance[i], 100*class_performance[i]/class_truth[i])
 end
-gnuplot.figure()
-gnuplot.plot({'Prediction',class_prediction},{'Truth',class_truth})
---gnuplot.raw('replot')
 
-gnuplot.figure()
-gnuplot.imagesc(model:get(1).output[13]);
-gnuplot.raw("set xrange restore")
+
+--PLOT THE TRUTH VS PREDICTION
+gnuplot.pngfigure("predvstruth.png")
+gnuplot.plot({'Prediction',class_prediction},{'Truth',class_truth})
+gnuplot.raw("replot")
+gnuplot.close()
+
+model:forward(train_data.data[18])
+--print(train_data.data[18][1])
+
+gnuplot.pngfigure("imageinput.png")
+--gnuplot.figure(221)
+gnuplot.imagesc(train_data.data[18][1])
 gnuplot.raw("unset border")
 gnuplot.raw("unset xtics")
 gnuplot.raw("unset ytics")
 gnuplot.raw("unset colorbox")
+gnuplot.title("input")
 gnuplot.raw('replot')
+gnuplot.close()
 
-gnuplot.figure()
-gnuplot.imagesc(model:get(3).output[13]);
+--merge 6 filtered outputs of first conv layer:
+spatToPlot1 = torch.cat(model:get(1).output[1],model:get(1).output[2],2)
+spatToPlot2 = torch.cat(model:get(1).output[3],model:get(1).output[4],2)
+spatToPlot3 = torch.cat(model:get(1).output[5],model:get(1).output[6],2)
+spatToPlot = torch.cat(spatToPlot1,spatToPlot2,1):cat(spatToPlot3,1)
+gnuplot.pngfigure("SpatConv1.png")
+--gnuplot.figure(222)
+gnuplot.imagesc(spatToPlot)
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.title("SpatialConvolution (First)")
+gnuplot.raw('replot')
+gnuplot.close()
 
-gnuplot.figure()
-gnuplot.imagesc(model:get(4).output[13]);
-gnuplot.figure()
-gnuplot.imagesc(model:get(6).output[13]);
 
---testing another optimization library. 
---trainer = nn.StochasticGradient(model, criterion)
---grads:zero()
---trainer.learningRate = 0.001
---trainer.maxIteration = 5
---trainer:train(train_data.data)
---outputopt2 = model:forward(test_data.data[13])
---print(outputopt2)
---how is the classes output ordered?
---for i=1,outputopt2:size(1) do
---   print(i, predicted[i])
---end
+--merge 6 filtered outputs of first trans layer:
+transToPlot1 = torch.cat(model:get(2).output[1],model:get(2).output[2],2)
+transToPlot2 = torch.cat(model:get(2).output[3],model:get(2).output[4],2)
+transToPlot3 = torch.cat(model:get(2).output[5],model:get(2).output[6],2)
+transToPlot = torch.cat(transToPlot1,transToPlot2,1):cat(transToPlot3,1)
+--gnuplot.figure(223)
+gnuplot.pngfigure("Trans1.png")
+gnuplot.imagesc(transToPlot)
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.title("Transfer (First)")
+gnuplot.raw('replot')
+gnuplot.close()
+
+--
+--merge 6 outputs of max pool layer
+poolToPlot1 = torch.cat(model:get(3).output[1],model:get(3).output[2],2)
+poolToPlot2 = torch.cat(model:get(3).output[3],model:get(3).output[4],2)
+poolToPlot3 = torch.cat(model:get(3).output[5],model:get(3).output[6],2)
+poolToPlot = torch.cat(poolToPlot1,poolToPlot2,1):cat(poolToPlot3,1)
+--gnuplot.figure(224)
+gnuplot.pngfigure("SpatMaxPool1.png")
+gnuplot.imagesc(poolToPlot)
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.title("SpatialMaxPooling (First)")
+gnuplot.raw('replot')
+gnuplot.close()
+
+--merge 6 filtered outputs of second conv layer:
+spatToPlot1 = torch.cat(model:get(4).output[1],model:get(4).output[2],2)
+spatToPlot2 = torch.cat(model:get(4).output[3],model:get(4).output[4],2)
+spatToPlot3 = torch.cat(model:get(4).output[5],model:get(4).output[6],2)
+spatToPlot = torch.cat(spatToPlot1,spatToPlot2,1):cat(spatToPlot3,1)
+--gnuplot.figure(225)
+gnuplot.pngfigure("SpatConv2.png")
+gnuplot.imagesc(spatToPlot)
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.title("SpatialConvolution (Second)")
+gnuplot.raw('replot')
+gnuplot.close()
+
+--merge 6 filtered outputs of second trans layer:
+transToPlot1 = torch.cat(model:get(5).output[1],model:get(5).output[2],2)
+transToPlot2 = torch.cat(model:get(5).output[3],model:get(5).output[4],2)
+transToPlot3 = torch.cat(model:get(5).output[5],model:get(5).output[6],2)
+transToPlot = torch.cat(transToPlot1,transToPlot2,1):cat(transToPlot3,1)
+--gnuplot.figure(226)
+gnuplot.pngfigure("Trans2.png")
+gnuplot.imagesc(transToPlot)
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.title("Transfer (Second)")
+gnuplot.raw('replot')
+gnuplot.close()
+
+--merge 6 outputs of second max pool layer
+poolToPlot1 = torch.cat(model:get(6).output[1],model:get(6).output[2],2)
+poolToPlot2 = torch.cat(model:get(6).output[3],model:get(6).output[4],2)
+poolToPlot3 = torch.cat(model:get(6).output[5],model:get(6).output[6],2)
+poolToPlot = torch.cat(poolToPlot1,poolToPlot2,1):cat(poolToPlot3,1)
+gnuplot.pngfigure("SpatMaxPool2.png")
+--gnuplot.figure(227)
+gnuplot.imagesc(poolToPlot)
+gnuplot.raw("unset border")
+gnuplot.raw("unset xtics")
+gnuplot.raw("unset ytics")
+gnuplot.raw("unset colorbox")
+gnuplot.title("SpatialMaxPooling (Second)")
+gnuplot.raw('replot')
+gnuplot.close()
+
